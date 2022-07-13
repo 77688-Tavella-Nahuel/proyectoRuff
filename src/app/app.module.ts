8355d02e23f58ccb2f7b9dd66b8e0b872f9ea6c3
77688 Nahuel Tavella <nahuelaso11@gmail.com>
2022-07-13 01:06:42 -0300

    Ruff
    Primer desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+// Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-icvoiao9.us.auth0.com',
+      clientId: 'O95MLV3Bv9UimAajDfup4VxADntMPtRv'
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
